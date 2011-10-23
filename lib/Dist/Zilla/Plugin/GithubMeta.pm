f650935f1dfc228751fba19e5f22d738c4728f83
Chris Williams <chris@bingosnet.co.uk>
2011-10-23 18:32:47 +0000

    Import of BINGOS/Dist-Zilla-Plugin-GithubMeta-0.26 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-GithubMeta
    gitpan-cpan-version:      0.26
    gitpan-cpan-path:         BINGOS/Dist-Zilla-Plugin-GithubMeta-0.26.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/GithubMeta.pm
+++ lib/Dist/Zilla/Plugin/GithubMeta.pm
@@ -1,6 +1,6 @@
 package Dist::Zilla::Plugin::GithubMeta;
 {
-  $Dist::Zilla::Plugin::GithubMeta::VERSION = '0.24';
+  $Dist::Zilla::Plugin::GithubMeta::VERSION = '0.26';
 }
 
 # ABSTRACT: Automatically include GitHub meta information in META.yml
@@ -166,7 +166,7 @@ Dist::Zilla::Plugin::GithubMeta - Automatically include GitHub meta information 
 
 =head1 VERSION
 
-version 0.24
+version 0.26
 
 =head1 SYNOPSIS
 
